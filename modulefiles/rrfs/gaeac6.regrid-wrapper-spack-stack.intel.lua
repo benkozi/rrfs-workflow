ec670d5db748d1a15f2cff5aa553a8af03eefccd
Ben Koziol <bkoziol@redlineperf.com>
2026-03-16 12:39:32 -0600

    update ld_library_path for gc6

--- modulefiles/rrfs/gaeac6.regrid-wrapper-spack-stack.intel.lua
+++ modulefiles/rrfs/gaeac6.regrid-wrapper-spack-stack.intel.lua
@@ -2,9 +2,12 @@ prepend_path("MODULEPATH", "/gpfs/f6/bil-fire8/world-shared/Benjamin.Koziol/mpas
 prepend_path("MODULEPATH", "/gpfs/f6/bil-fire8/world-shared/Benjamin.Koziol/mpas-aerosols/spack-stack/envs/mpas-aerosols/modules/cray-mpich/8.1.32/intel-oneapi-compilers/2025.2.1")
 prepend_path("MODULEPATH", "/gpfs/f6/bil-fire8/world-shared/Benjamin.Koziol/mpas-aerosols/spack-stack/envs/mpas-aerosols/modules/intel-oneapi-compilers/2025.2.1")
 
+prepend_path("LD_LIBRARY_PATH", "/opt/intel/oneapi/mkl/2025.2/lib")
+
 load("stack-intel-oneapi-compilers/2025.2.1")
 load("stack-cray-mpich/8.1.32")
 
+load("py-numpy/1.26.4")
 load("esmf/8.9.1")
 load("py-netcdf4/1.7.2")
 load("py-pytest/8.2.1")
