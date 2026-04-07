502bbefa6082ee23a72e4a336cfb93f89e5c6e24
Ben Koziol <bkoziol@redlineperf.com>
2026-04-07 12:08:36 -0600

    add py-pydantic-settings to modules

--- modulefiles/rrfs/gaeac6.regrid-wrapper-spack-stack.intel.lua
+++ modulefiles/rrfs/gaeac6.regrid-wrapper-spack-stack.intel.lua
@@ -13,6 +13,7 @@ load("py-netcdf4/1.7.2")
 load("py-pytest/8.2.1")
 load("py-xarray/2024.7.0")
 load("py-pydantic/2.10.1")
+load("py-pydantic-settings/2.6.1")
 
 load("Core/24.11")
 load("nco/5.1.9")
