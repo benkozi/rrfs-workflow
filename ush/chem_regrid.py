5dbdca48024e0ce9b084d6a1d6f418ffbf00460e
Ben Koziol <bkoziol@redlineperf.com>
2026-04-09 10:06:43 -0600

    fix indexing logic

--- ush/chem_regrid.py
+++ ush/chem_regrid.py
@@ -30,10 +30,12 @@ def main() -> None:
 
     try:
         data["scrip_path"] = sys.argv[7]  # Path to the input SCRIP/UGRID domain grid file
-        data["dst_path"] = sys.argv[8]  # Path to the destination grid (e.g., init.nc)
+        try:
+            data["dst_path"] = sys.argv[8]  # Path to the destination grid (e.g., init.nc)
+        except IndexError:
+            data["dst_path"] = None
     except IndexError:
         data["scrip_path"] = None
-        data["dst_path"] = None
 
     ctx = ChemRegridContext.model_validate(data)
     LOGGER.info(f"{ctx.model_dump_json(indent=2)=}")
